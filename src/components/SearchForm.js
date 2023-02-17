e12b5e8ce39669654e442310a3f6eca89bd0fcb7
Kirill Golubjov <kirillcnz@gmail.com>
2023-02-17 15:15:40 +0200

    display list & search form

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,9 +1,37 @@
 import React from 'react';
 import { useGlobalContext } from '../context';
+import { useEffect, useRef } from 'react';
 
 const SearchForm = () => {
   const { setSearchTerm } = useGlobalContext();
-  return <form action=''></form>;
+  const searchValue = useRef('');
+
+  useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <section className='section search'>
+      <form className='search-form' onSubmit={handleSubmit}>
+        <div className='form-control'>
+          <label htmlFor='name'>search your favorite cocktail</label>
+          <input
+            type='text'
+            id='name'
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default SearchForm;
