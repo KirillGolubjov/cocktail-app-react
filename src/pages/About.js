8f4c7a642ed8524df335a6f0423aeef0a4212c78
Kirill Golubjov <kirillcnz@gmail.com>
2023-02-17 12:56:58 +0200

    Navbar, About and Error

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum
+        debitis iure delectus magni nihil, mollitia, facilis sit autem explicabo
+        voluptates velit. Atque cumque dolor, quae harum earum assumenda!
+        Adipisci, aperiam?
+      </p>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CocktailList from '../components/CocktailList';
+import SearchForm from '../components/SearchForm';
+
+const Home = () => {
+  return (
+    <>
+      <CocktailList />
+      <SearchForm />
+    </>
+  );
+};
+
+export default Home;
